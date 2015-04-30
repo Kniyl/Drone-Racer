15f34070dce141aaa7ea3fd2c5a57c81c484ee68
Mathias <mathias@mettinger.fr>
2015-04-30 11:44:47 +0200

    Improve program invocation options

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ import drone_racer
 parser = ArgumentParser(description='Interface graphique "Drone Racer"')
 parser.add_argument('--fancy-title', dest='fancy', action='store_true',
                     help='Utilise une barre de titre un peu plus Gtk3')
+parser.add_argument('--serial-port', dest='serial', metavar='FILE',
+                    default=None, help='Spécifie le port série à utiliser '
+                    'pour récupérer les informations provenant du XBee')
+parser.add_argument('--zigbee', dest='zigbee', action='store_true',
+                    help='Spécifie si le module XBee est un ZigBee')
 args = parser.parse_args()
-reader = drone_racer.XBeeReader('/dev/ttyAMA0', baudrate=115200)
+if args.serial is not None:
+    reader = drone_racer.XBeeReader(
+            args.serial, baudrate=115200, zigbee=args.zigbee)
+else:
+    reader = drone_racer.StdInReader
 app = drone_racer.Application(reader, args.fancy)
 app.run()
