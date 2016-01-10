294642407b8e94b0bbd95d14bceab7a97976bd8d
Mathias Ettinger <mathias@mettinger.fr>
2016-01-10 23:19:54 +0100

    Improved gettext integration

--- drone_racer/i18n.py
+++ drone_racer/i18n.py
@@ -2,7 +2,9 @@ import os.path
 import gettext
 
 def translations(domain):
-    locales_dir = os.path.abspath(os.path.dirname(__file__))
-    translation = gettext.translation(domain, locales_dir)
+    locales_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+    locales_dir = os.path.join(locales_dir, 'resources', 'locales')
+    # We do want NullTranslations instance for unsupported languages
+    translation = gettext.translation(domain, locales_dir, fallback=True)
     return translation.gettext, translation.ngettext
 
